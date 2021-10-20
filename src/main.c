262825231e38bf7b772b120eff7b7a65d319a60f
David R <egoz3ntrum@gmail.com>
2021-10-20 23:31:54 +0000

    darodrig

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
 #include "../inc/famine.h"
 
-
 int main(int argc, char **argv)
 {
     DIR *test;
-    DIR *test2;
+    struct dirent *dir;
 
     if (argc != 1)
         return (-1);
@@ -12,14 +11,21 @@ int main(int argc, char **argv)
     test = opendir("/tmp/test");
     if (test)
     {
+        while ((dir = readdir(test)) && \
+               strcmp(".", dir->d_name) && \
+               strcmp("..", dir->d_name))
+            debug("/tmp/test/%s", dir->d_name);
         closedir(test);
-    } else
-        debug("%s","test folder not found");
-    test2 = opendir("/tmp/test2");
-    if (test2)
+    }
+    else
+        debug("%s", "test folder not found");
+    test = opendir("/tmp/test2/");
+    if (test)
     {
-        closedir(test2);
-    } else 
-        dprintf(2, "error opening /tmp/test2 folder");
-
+        while ((dir = readdir(test)) && \
+               strcmp(".", dir->d_name) && \
+               strcmp("..", dir->d_name))
+            debug("/tmp/test2/%s", dir->d_name);
+        closedir(test);
+    }
 }
